53120cd7d9bf25b857a0fb88ef0d62533f41f013
gueretamartin <guereta.martin@gmail.com>
2016-04-25 18:01:58 -0300

    TP2 LAB 02 (no terminado, faltan completar clases del diagrama de clases)

---
+++ Business.Entities/ModuloUsuario.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class ModuloUsuario : BusinessEntity
+    {
+        int idUsuario;
+        int idModulo;
+        bool permiteAlta;
+        bool permiteBaja;
+        bool permiteModificacion;
+        bool permiteConsulta;
+
+        public int IdUsuario
+        {
+            get
+            {
+                return idUsuario;
+            }
+
+            set
+            {
+                idUsuario = value;
+            }
+        }
+
+        public int IdModulo
+        {
+            get
+            {
+                return idModulo;
+            }
+
+            set
+            {
+                idModulo = value;
+            }
+        }
+
+        public bool PermiteAlta
+        {
+            get
+            {
+                return permiteAlta;
+            }
+
+            set
+            {
+                permiteAlta = value;
+            }
+        }
+
+        public bool PermiteBaja
+        {
+            get
+            {
+                return permiteBaja;
+            }
+
+            set
+            {
+                permiteBaja = value;
+            }
+        }
+
+        public bool PermiteModificacion
+        {
+            get
+            {
+                return permiteModificacion;
+            }
+
+            set
+            {
+                permiteModificacion = value;
+            }
+        }
+
+        public bool PermiteConsulta
+        {
+            get
+            {
+                return permiteConsulta;
+            }
+
+            set
+            {
+                permiteConsulta = value;
+            }
+        }
+    }
+}
